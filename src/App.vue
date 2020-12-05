f5d4e4fa53e8104745a8bf812901b2f41de60ee3
Nikita <30903256+NikitosGolubev@users.noreply.github.com>
2020-12-05 14:08:16 +0300

    ESlint set-up and configuration

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">
+        Home
+      </router-link> |
+      <router-link to="/about">
+        About
+      </router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+  /* eslint-disable vue-scoped-css/require-scoped */
+</script>
 
 <style lang="scss">
 #app {
